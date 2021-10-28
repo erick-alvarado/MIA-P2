114bc23d8071300e54d8184d306961b6f7d88c68
erick-alvarado <58530492+erick-alvarado@users.noreply.github.com>
2021-10-28 17:05:31 -0600

    Auth server

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,27 @@
 //node src/index.js
 //npm run server
+const { requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
 const express = require('express');
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express();
 const loadenv = require('dotenv').config()
 const loginRouter = require('../routes/login.js')
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.AUTH_BASE_URL,
+  clientID: process.env.AUTH_CLIENT_ID,
+  issuerBaseURL:  process.env.AUTH_ISSUER
+};
+
+
+app.use(auth(config));
+
 
 app.set('port',process.env.PORT_NODE)
 
@@ -14,8 +30,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors())
 
-app.use("/login", loginRouter)
+//app.use("/login", loginRouter)
 
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+  console.log(req.oidc.user)
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get("port")}`);
