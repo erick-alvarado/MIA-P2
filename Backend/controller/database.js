88f25ec854efaa88d3c3d941c78a71ed4bd426d1
erick-alvarado <58530492+erick-alvarado@users.noreply.github.com>
2021-11-01 20:58:52 -0600

    Carga/Categoria

--- Backend/controller/database.js
+++ Backend/controller/database.js
@@ -1,7 +1,7 @@
 const db_ = require('../config/config');
 
 exports.cargaMasiva = async(req,res)=>{
-    var json_  = req.body.departamentos.departamento;
+    let json_  = req.body.departamentos.departamento;
 
     if(json_.nombre){
         await departamento('null',json_)
@@ -16,17 +16,17 @@ exports.cargaMasiva = async(req,res)=>{
 }
 
 async function departamento(id_padre,dep){
-    sql = `insert into departamento(id_departamento_aux,nombre,capital) select ${id_padre}, '${dep.nombre}',${dep.capital_total} from dual
+    let sql = `insert into departamento(id_departamento_aux,nombre,capital) select ${id_padre}, '${dep.nombre}',${dep.capital_total} from dual
         where not exists(select * from departamento where (
             nombre = '${dep.nombre}' 
         ))`;
-    res = await execute(sql)
+    let res = await execute(sql)
 
     if(dep.puestos){
-        json_= dep.puestos.puesto;
-        sql = `select id_departamento from departamento where nombre = '${dep.nombre}'`
-        res = await execute(sql)
-        id_dep_aux = res.rows[0][0]
+        let json_= dep.puestos.puesto;
+        let sql = `select id_departamento from departamento where nombre = '${dep.nombre}'`
+        let res = await execute(sql)
+        let id_dep_aux = res.rows[0][0]
         if(json_.nombre){
             await puesto(id_dep_aux,json_)
         }
@@ -38,20 +38,38 @@ async function departamento(id_padre,dep){
     }
 }
 async function puesto(id_dep, puesto){
-    imagen  = 'null'
+    let imagen  = 'null'
     if(puesto.imagen){
         imagen = `'${puesto.imagen}'`
     }
-    sql = `insert into puesto(id_puesto_departamento, nombre,salario,calificacion,url_imagen) 
+    let sql = `insert into puesto(id_puesto_departamento, nombre,salario,calificacion,url_imagen) 
         select ${id_dep}, '${puesto.nombre}',${puesto.salario},0,${imagen} from dual
         where not exists(select * from puesto where (
             nombre = '${puesto.nombre}' and
             id_puesto_departamento = ${id_dep}
         ))`;
-    console.log(sql)
-    res = await execute(sql)
+    let res = await execute(sql)
+
+    if(puesto.categorias){
+        let json_= puesto.categorias.categoria;
+        if(json_.nombre){
+            await categoria(json_)
+        }
+        else{
+            for(var k in json_) {
+                await categoria(json_[k])
+            }
+        }
+    }
+}
+async function categoria(cat){
+    let sql = `insert into categoria(nombre) select '${cat.nombre}' from dual
+        where not exists(select * from categoria where (
+            nombre = '${cat.nombre}' 
+        ))`;
+    let res = await execute(sql)
 }
 async function execute(query){
-    let result = await db_.Open(sql,[],true).catch((e) => { console.error(e);})
+    let result = await db_.Open(query,[],true).catch((e) => { console.error(e);})
     return result;
 }
